d18ef6b3c4af7fd7cc6146237631d30ce307e1d8
muthubala-code <mohan144004@gmail.com>
2025-08-12 19:03:48 +0530

    feat(Auth): add google oauth and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <StrictMode>
+      <BrowserRouter>
+        <GoogleOAuthProvider clientId="240049511783-nj9fbj759jm5hetqt2cn2bbel17atphl.apps.googleusercontent.com">
+          <App />
+        </GoogleOAuthProvider>
+      </BrowserRouter>
+    </StrictMode>
+  </Provider>
+);
