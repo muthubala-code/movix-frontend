4c19756473f3cc89b1231003bf0972ad80582406
muthubala-code <mohan144004@gmail.com>
2025-08-12 21:01:04 +0530

    fix(main): fix navbar logo and styles

--- src/components/navbar/NavBarLeft.jsx
+++ src/components/navbar/NavBarLeft.jsx
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom";
 import { navItems } from "./navItems";
 import "./styles/navbarLeft.css";
 
 const NavBarLeft = () => {
   return (
     <div className="navbar-left">
-      <div className="logo">
-        <img src="/logo.png" alt="logo" />
-      </div>
+      <Link className="logo" to="/">
+        <img src="/icon.png" alt="logo" />
+        <h2>Movix</h2>
+      </Link>
 
       <div className="navbar-links-container">
         <ul className="navbar-links">
-          {navItems.map((item,index) => (
+          {navItems.map((item, index) => (
             <li className="navbar-link" key={index}>
               <a href={item.navUrl}>{item.navItem}</a>
             </li>
